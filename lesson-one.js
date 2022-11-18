b4f8bce4b6873547f0b5b1c91d7d18802e3120f7
devin-lynch <devin.l.lynch@gmail.com>
2022-11-17 22:49:40 -0600

    Continue lesson

--- lesson-one.js
+++ lesson-one.js
@@ -1,3 +1,4 @@
+"use strict";
 // TYPESCRIPT LESSON
 // let firstName = 'Devin';
 // console.log(firstName.toUpperCase());
@@ -12,8 +13,29 @@
 // } else {
 //   phoneNumber = '7167762323';
 // }
-var band;
-band = 'Hop Along';
-console.log(band);
-band = 'Actually Im a Ghost';
-console.log(band);
+// let band : string;
+// band = 'Hop Along'
+// console.log(band)
+// band = 'Actually Im a Ghost'
+// console.log(band)
+// function triple(value : number) {
+//     return value * 3;
+//   }
+//   function greetTripled(greeting : string, value : number) {
+//     console.log(`${greeting}, ${triple(value)}!`);
+//   }
+//   greetTripled('Hiya', 5);
+// function proclaim(status?: string) {
+//     console.log(`I'm ${status || 'not ready...'}`);
+//   }
+//   proclaim();
+//   proclaim('ready?');
+//   proclaim('ready!');
+function proclaim(status = 'not ready...', repeat = 1) {
+    for (let i = 0; i < repeat; i += 1) {
+        console.log(`I'm ${status}`);
+    }
+}
+proclaim();
+proclaim('ready?');
+proclaim('ready!', 3);
--- lesson-one.ts
+++ lesson-one.ts
@@ -1,17 +1,20 @@
 // TYPESCRIPT LESSON
 
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
+
 // let firstName = 'Devin';
 
 // console.log(firstName.toUpperCase());
 // console.log(firstName.length);
 // console.log(firstName);
 
-
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
 
 // let guess;
 // guess = 'green';
 // guess = 2;
 
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
 
 // let phoneNumber : string;
 
@@ -21,9 +24,49 @@
 //   phoneNumber = '7167762323';
 // }
 
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
 
 // let band : string;
 // band = 'Hop Along'
 // console.log(band)
 // band = 'Actually Im a Ghost'
 // console.log(band)
+
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
+
+// function triple(value : number) {
+//     return value * 3;
+//   }
+  
+//   function greetTripled(greeting : string, value : number) {
+//     console.log(`${greeting}, ${triple(value)}!`);
+//   }
+  
+//   greetTripled('Hiya', 5);
+
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
+
+// function proclaim(status?: string) {
+//     console.log(`I'm ${status || 'not ready...'}`);
+//   }
+  
+//   proclaim();
+//   proclaim('ready?');
+//   proclaim('ready!');
+
+// -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
+
+// function proclaim(status = 'not ready...', repeat = 1) {
+//     for (let i = 0; i < repeat; i += 1) {
+//       console.log(`I'm ${status}`);
+//     }
+//   }
+  
+//   proclaim();
+//   proclaim('ready?');
+//   proclaim('ready!', 3);
+
+  // -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - --
+
+
+  
